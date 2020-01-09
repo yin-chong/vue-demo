f11847bc8490cfa43bc033aad5904a65953ac268
xin.jiang <xin.jiang@ximalaya.com>
2020-01-09 10:58:57 +0800

    update

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -13,7 +13,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
   //import BMap from 'BMap'
   // import axios from "axios";
   export default {
@@ -53,63 +52,7 @@
 
     },
     mounted () {
-=======
-//import BMap from 'BMap'
-// import axios from "axios";
-export default {
-  name: "Map",
-  data() {
-    return {
-      pointA: {}, // 我的经纬度
-      query: "" // 查询
-    };
-  },
-  created() {
-    //  const geolocation = new BMap.Geolocation({
-    //      maximumAge:10  // 清除缓存
-    //  });
-    //  geolocation.getCurrentPosition((res) =>{
-    //    alert(res)
-    //    console.log(res);
-    //  this.pointA = new BMap.Point(res.point.lng, res.point.lat)  // 通过浏览器获得我的经纬度
-    //  console.log(this.pointA);
-    //  this.createMap()
-    //  })
-    this.$jsonp(
-      "http://api.map.baidu.com/location/ip?qt=loc&coor=bd09ll&ak=xY7kZjVZdY2rL5brH2UyBuUdN3uYIKYG&timeout=10000"
-    ).then(res => {
-      console.log(res);
-    });
-  },
-  mounted() {},
-  methods: {
-    createMap() {
-      //  创建地图实例
-      const map = new BMap.Map("allmap", {
-        enableBizAuthLogo: false // 初始化地图时进行关闭设置
-      });
-      //    map.disableBizAuthLogo(); // 关闭地图
-      //    map.enableBizAuthLogo(); // 开启地图
-      //  创建点坐标  (中航科技大厦)
-      const point = new BMap.Point(118.810742, 32.04179);
-      // 地图展示级别和中心点坐标
-      map.centerAndZoom(point, 15);
-      // 运行滚轮缩放
-      map.enableScrollWheelZoom(true);
-      //    设置地图显示的城市
-      map.setCurrentCity("南京");
-      const point1 = new BMap.Point(this.pointA.lng, this.pointA.lat);
-      const distance = map.getDistance(point1, point).toFixed(2); //保留2位小数
-      console.log(distance);
-      alert(distance);
-      // 将2点用线连接显示在地图上
-      const polyline = new BMap.Polyline([point1, point], {
-        strokeColor: "blue",
-        strokeWeight: 6,
-        strokeOpacity: 0.5
-      });
-      map.addOverlay(polyline);
->>>>>>> 5ff99d994fc0c8a978912432485e94cbe307d605
+
     },
 
     watch:  {
