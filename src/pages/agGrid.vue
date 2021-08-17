ba1db707c60df616b0ba1662078829ff423e75d6
yinchong@copote.com <yinchong@copote.com>
2021-08-17 18:06:35 +0800

    AG Grid 编辑表头

--- src/pages/agGrid.vue
+++ src/pages/agGrid.vue
@@ -21,8 +21,9 @@
 
 <script>
 import { AgGridVue } from "ag-grid-vue";
-import { localeText } from "../config/ap-grid-chinese-config";
-import "ag-grid-enterprise";
+// 汉化
+import { localeText } from "@/config/ap-grid-chinese-config";
+import "ag-grid-community";
 export default {
   name: "ag-grid",
   data() {
@@ -39,30 +40,60 @@ export default {
     AgGridVue,
   },
   beforeMount() {
+    // this.columnDefs = [
+    //   { headerName: "多选", checkboxSelection: true },
+    //   { field: "make", headerName: "姓名", sortable: true, filter: true },
+    //   { field: "model", headerName: "商品", sortable: true, filter: true },
+    //   { field: "price", headerName: "价格", sortable: true, filter: true },
+    // ];
+    /**
+     * @description 多表头
+     * @param resizable 调整列的大小
+     * @param checkboxSelection 选择框  rowSelection="multiple"  多选
+     * @param sortable 分组
+     * @param filter 筛选 agNumberColumnFilter 数字类型过滤器 agTextColumnFilter 字符串类型 agDateColumnFilter 时间类型
+     * @param floatingFilter 是否直接显示过滤器
+     * @param cellEditor 表格编辑
+     */
     this.columnDefs = [
-      { headerName: "多选", checkboxSelection: true },
-      { field: "make", headerName: "姓名", sortable: true, filter: true },
-      { field: "model", headerName: "商品", sortable: true, filter: true },
-      { field: "price", headerName: "价格", sortable: true, filter: true },
+      {
+        headerName: "分组A",
+        children: [
+          { headerName: "多选", checkboxSelection: true },
+          { field: "make", headerName: "姓名", sortable: true, filter: true, resizable:true, cellEditor: 'agLargeTextCellEditor', },
+          { field: "model", headerName: "商品", sortable: true, filter: true },
+        ],
+      },
+      {
+        headerName: "分组B",
+        children: [
+          { field: "price", headerName: "价格", sortable: true, filter: 'agNumberColumnFilter',  floatingFilter: true },
+          { headerName: "籍贯", field: "jg" },
+          { headerName: "省份", field: "sf" },
+          { headerName: "地址", field: "dz" },
+        ],
+      },
     ];
 
-    this.autoGroupColumnDef = {
-      headerName: "Model",
-      field: "model",
-      cellRenderer: "agGroupCellRenderer",
-      cellRendererParams: {
-        checkbox: true,
-      },
-    };
+    // this.autoGroupColumnDef = {
+    //   headerName: "Model",
+    //   field: "model",
+    //   cellRenderer: "agGroupCellRenderer",
+    //   cellRendererParams: {
+    //     checkbox: true,
+    //   },
+    // };
     fetch("https://www.ag-grid.com/example-assets/row-data.json")
       .then((result) => result.json())
       .then((rowData) => (this.rowData = rowData));
   },
   methods: {
-    // 表格 checkbox
+    // ag-grid创建完成后执行的事件
     onGridReady(params) {
       this.gridApi = params.api;
       this.columnApi = params.columnApi;
+      // 自适应大小
+      this.gridApi.sizeColumnsToFit();
     },
     //   按钮 点击查看选中的行
     getSelectRows() {
